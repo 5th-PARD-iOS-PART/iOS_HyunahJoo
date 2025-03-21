1d3813bf45bc860583037de18ba8120746990403
주현아 <gianna@juhyeon-aui-MacBookPro.local>
2025-03-21 19:40:49 +0900

    new folders and files are created

---
+++ 1st_hw_JooHyunah/1st_hw_JooHyunah/_st_hw_JooHyunahApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_hw_JooHyunahApp.swift
+//  1st_hw_JooHyunah
+//
+//  Created by 주현아 on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_hw_JooHyunahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 1st_hw_JooHyunah/1st_hw_JooHyunahTests/_st_hw_JooHyunahTests.swift
@@ -0,0 +1,16 @@
+//
+//  _st_hw_JooHyunahTests.swift
+//  1st_hw_JooHyunahTests
+//
+//  Created by 주현아 on 3/21/25.
+//
+
+import Testing
+
+struct _st_hw_JooHyunahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
