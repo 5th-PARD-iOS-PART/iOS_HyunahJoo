314958a64cc93a42e949c7005f68ef4e465e11be
주현아 <gianna@juhyeon-aui-MacBookPro.local>
2025-03-23 22:23:20 +0900

    files are created

---
+++ 2nd_hw_JooHyunah/2nd_hw_JooHyunah/_nd_hw_JooHyunahApp.swift
@@ -0,0 +1,17 @@
+//
+//  _nd_hw_JooHyunahApp.swift
+//  2nd_hw_JooHyunah
+//
+//  Created by 주현아 on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct _nd_hw_JooHyunahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 2nd_hw_JooHyunah/2nd_hw_JooHyunahTests/_nd_hw_JooHyunahTests.swift
@@ -0,0 +1,16 @@
+//
+//  _nd_hw_JooHyunahTests.swift
+//  2nd_hw_JooHyunahTests
+//
+//  Created by 주현아 on 3/22/25.
+//
+
+import Testing
+
+struct _nd_hw_JooHyunahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
