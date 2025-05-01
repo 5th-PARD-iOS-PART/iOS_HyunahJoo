06b63cf18b0e4603ba4f980e87fc578bee4541ad
주현아 <gianna@juhyeon-aui-MacBookPro.local>
2025-05-01 19:18:06 +0900

    files are created

---
+++ 5th_hw_JooHyunah/5th_hw_JooHyunah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  5th_hw_JooHyunah
+//
+//  Created by 주현아 on 5/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 5th_hw_JooHyunah/5th_hw_JooHyunah/_th_hw_JooHyunahApp.swift
@@ -0,0 +1,17 @@
+//
+//  _th_hw_JooHyunahApp.swift
+//  5th_hw_JooHyunah
+//
+//  Created by 주현아 on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct _th_hw_JooHyunahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
