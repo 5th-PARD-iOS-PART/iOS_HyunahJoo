c8df385aa8f59afbaf2bce11dfa6ad19b25ff7c4
gianna <cheu8340@gmail.com>
2025-06-23 20:17:52 +0900

    files are created

---
+++ Additional Seminar/Additional Seminar/Additional_SeminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Additional_SeminarApp.swift
+//  Additional Seminar
+//
+//  Created by 주현아 on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Additional_SeminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Additional Seminar/Additional SeminarTests/Additional_SeminarTests.swift
@@ -0,0 +1,16 @@
+//
+//  Additional_SeminarTests.swift
+//  Additional SeminarTests
+//
+//  Created by 주현아 on 6/23/25.
+//
+
+import Testing
+
+struct Additional_SeminarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
